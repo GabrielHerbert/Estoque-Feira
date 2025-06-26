5a60dc89bb99c919aab249f5c3d93a4e47aca4d5
GabrielHerbert <35049841+GabrielHerbert@users.noreply.github.com>
2025-06-26 17:04:29 -0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -43,7 +43,7 @@ logger = logging.getLogger(__name__)
 ) = range(12)
 
 # Token do bot
-TOKEN = "7952211429:AAHP8UFrrmd_E96QIZQNwZigd5zjK8VN5QY"
+TOKEN = ""
 
 # Lista de IDs de usuários autorizados a usar comandos administrativos
 ADMIN_IDS = [
